c1a16c2d7c829686630982cb268f5612292dde6a
Jordan Jordanov <d_yugioh@abv.bg>
2023-04-04 09:29:14 +0300

    v2023

---
+++ src/components/Form.js
@@ -0,0 +1,28 @@
+import * as React from "react";
+import axios from "axios";
+
+class Form extends React.Component {
+    state = { userName: '' };
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
+        this.props.onSubmit(resp.data);
+        this.setState({ userName: '' });
+    };
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    type="text"
+                    value={this.state.userName}
+                    onChange={event => this.setState({ userName: event.target.value })}
+                    placeholder="GitHub username"
+                    required
+                />
+                <button>Add card</button>
+            </form>
+        );
+    }
+}
+
+export default Form;
