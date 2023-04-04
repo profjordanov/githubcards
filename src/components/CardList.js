147ed40515f88ee96451bab061dcd445688f91bd
Jordan Jordanov <d_yugioh@abv.bg>
2023-04-04 07:31:03 +0300

    v2023

--- src/components/CardList.js
+++
@@ -1,9 +0,0 @@
-import Card from "./Card";
-
-const CardList = (props) => (
-    <div>
-        {props.profiles.map(profile => <Card key={profile.id} {...profile}/>)}
-    </div>
-);
-
-export default CardList;
