390e29aa2d8beec371ec85efde43f6efc5a4b3a2
profjordanov <d_yugioh@abv.bg>
2025-12-28 16:38:33 +0200

    Clean up for 2026

--- src/components/CardList.jsx
+++
@@ -1,9 +0,0 @@
-import Card from "./Card";
-
-const CardList = ({profiles}) => (
-    <div className="card-deck">
-        {profiles.map(profile => <Card key={profile.id} profile={profile}/>)}
-    </div>
-);
-
-export default CardList;
